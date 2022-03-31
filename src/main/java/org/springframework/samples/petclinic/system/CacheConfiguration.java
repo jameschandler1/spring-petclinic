03d8224615de520ceb7dc537c1e6a72053127764
James Chandler <stillsound@protonmail.com>
2022-03-31 10:56:39 -0700

    edit

--- src/main/java/org/springframework/samples/petclinic/system/CacheConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/system/CacheConfiguration.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2012-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.samples.petclinic.system;
 
 import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
@@ -28,6 +12,7 @@ import javax.cache.configuration.MutableConfiguration;
  * creates the used cache for the application and enables statistics that become
  * accessible via JMX.
  */
+
 @Configuration(proxyBeanMethods = false)
 @EnableCaching
 class CacheConfiguration {
@@ -48,6 +33,7 @@ class CacheConfiguration {
 	 * configuration options (like the size limit) must be set via a configuration
 	 * mechanism that is provided by the selected JCache implementation.
 	 */
+
 	private javax.cache.configuration.Configuration<Object, Object> cacheConfiguration() {
 		return new MutableConfiguration<>().setStatisticsEnabled(true);
 	}
